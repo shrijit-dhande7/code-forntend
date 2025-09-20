0d09492e4e316679b2362ea91b188f9d323ddf78
shrijit-dhande7 <shrijitdhande@gmail.com>
2025-09-20 15:02:38 +0530

    Update axiosClient.js

--- src/utils/axiosClient.js
+++ src/utils/axiosClient.js
@@ -1,20 +1,12 @@
-import axios from "axios";
+import axios from "axios"
 
-const axiosClient = axios.create({
-  baseURL: "https://code-backend-zeta.vercel.app",
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
+const axiosClient =  axios.create({
+    baseURL: 'https://code-backend-zeta.vercel.app',
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json'
+    }
 });
 
-// 🔑 Add interceptor to attach token from localStorage
-axiosClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); // adjust key name if different
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
 
 export default axiosClient;
